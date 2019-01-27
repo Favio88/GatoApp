fd00f70e5f421d7990aa0e1930f7299064b78b0e
Favio Orlando <favio_galarza@hotmail.com>
2019-01-27 14:07:40 -0600

    Segundo commit

--- app/src/main/java/com/favio/gatoapp/GatoActivity.java
+++ app/src/main/java/com/favio/gatoapp/GatoActivity.java
@@ -4,18 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class GatoActivity extends AppCompatActivity {
 
-    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
-    boolean turno=false;
-
+    Button btnJugar, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+    String valorBtn1=" ", valorBtn2=" ", valorBtn3=" ", valorBtn4=" ", valorBtn5=" ", valorBtn6=" ",
+            valorBtn7=" ", valorBtn8=" ", valorBtn9=" ";
+    TextView txtv_ganador;
+    boolean turno;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gato);
 
+        turno=false;
+
+        btnJugar=findViewById(R.id.btnJugar);
         btn1=findViewById(R.id.btn1);
         btn2=findViewById(R.id.btn2);
         btn3=findViewById(R.id.btn3);
@@ -26,38 +33,286 @@ public class GatoActivity extends AppCompatActivity {
         btn8=findViewById(R.id.btn8);
         btn9=findViewById(R.id.btn9);
 
+        btn1.setEnabled(false);
+        btn2.setEnabled(false);
+        btn3.setEnabled(false);
+        btn4.setEnabled(false);
+        btn5.setEnabled(false);
+        btn6.setEnabled(false);
+        btn7.setEnabled(false);
+        btn8.setEnabled(false);
+        btn9.setEnabled(false);
+
+        txtv_ganador = findViewById(R.id.txtv_ganador);
+
+        btnJugar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                turno=false;
+
+                btn1.setEnabled(true);
+                btn2.setEnabled(true);
+                btn3.setEnabled(true);
+                btn4.setEnabled(true);
+                btn5.setEnabled(true);
+                btn6.setEnabled(true);
+                btn7.setEnabled(true);
+                btn8.setEnabled(true);
+                btn9.setEnabled(true);
+
+                btn1.setText(" ");
+                btn2.setText(" ");
+                btn3.setText(" ");
+                btn4.setText(" ");
+                btn5.setText(" ");
+                btn6.setText(" ");
+                btn7.setText(" ");
+                btn8.setText(" ");
+                btn9.setText(" ");
+
+                Toast.makeText(GatoActivity.this, "Turno Jugador 1", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+
 
                 if(turno == false){
                     btn1.setText("X");
                     turno=true;
                     btn1.setEnabled(false);
+                    Toast.makeText(GatoActivity.this, "Turno Jugador 2", Toast.LENGTH_SHORT).show();
                 }else{
                     btn1.setText("O");
                     turno=false;
                     btn1.setEnabled(false);
+                    Toast.makeText(GatoActivity.this, "Turno Jugador 1", Toast.LENGTH_SHORT).show();
                 }
+
+                btn1 = (Button) v;
+                valorBtn1 = btn1.getText().toString();
             }
         });
 
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
 
                 if(turno == false){
-                    btn1.setText("X");
+                    btn2.setText("X");
                     turno=true;
-                    btn1.setEnabled(false);
+                    btn2.setEnabled(false);
+                    Toast.makeText(GatoActivity.this, "Turno Jugador 2", Toast.LENGTH_SHORT).show();
                 }else{
-                    btn1.setText("O");
+                    btn2.setText("O");
                     turno=false;
-                    btn1.setEnabled(false);
+                    btn2.setEnabled(false);
+                    Toast.makeText(GatoActivity.this, "Turno Jugador 1", Toast.LENGTH_SHORT).show();
                 }
+
+                btn2 = (Button) v;
+                valorBtn2 = btn2.getText().toString();
             }
         });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(turno == false){
+                    btn3.setText("X");
+                    turno=true;
+                    btn3.setEnabled(false);
+                    Toast.makeText(GatoActivity.this, "Turno Jugador 2", Toast.LENGTH_SHORT).show();
+                }else{
+                    btn3.setText("O");
+                    turno=false;
+                    btn3.setEnabled(false);
+                    Toast.makeText(GatoActivity.this, "Turno Jugador 1", Toast.LENGTH_SHORT).show();
+                }
+
+                btn3 = (Button) v;
+                valorBtn3 = btn3.getText().toString();
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(turno == false){
+                    btn4.setText("X");
+                    turno=true;
+                    btn4.setEnabled(false);
+                    Toast.makeText(GatoActivity.this, "Turno Jugador 2", Toast.LENGTH_SHORT).show();
+                }else{
+                    btn4.setText("O");
+                    turno=false;
+                    btn4.setEnabled(false);
+                    Toast.makeText(GatoActivity.this, "Turno Jugador 1", Toast.LENGTH_SHORT).show();
+                }
+
+                btn4 = (Button) v;
+                valorBtn4 = btn4.getText().toString();
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(turno == false){
+                    btn5.setText("X");
+                    turno=true;
+                    btn5.setEnabled(false);
+                    Toast.makeText(GatoActivity.this, "Turno Jugador 2", Toast.LENGTH_SHORT).show();
+                }else{
+                    btn5.setText("O");
+                    turno=false;
+                    btn5.setEnabled(false);
+                    Toast.makeText(GatoActivity.this, "Turno Jugador 1", Toast.LENGTH_SHORT).show();
+                }
+
+                btn5 = (Button) v;
+                valorBtn5 = btn5.getText().toString();
+            }
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(turno == false){
+                    btn6.setText("X");
+                    turno=true;
+                    btn6.setEnabled(false);
+                    Toast.makeText(GatoActivity.this, "Turno Jugador 2", Toast.LENGTH_SHORT).show();
+                }else{
+                    btn6.setText("O");
+                    turno=false;
+                    btn6.setEnabled(false);
+                    Toast.makeText(GatoActivity.this, "Turno Jugador 1", Toast.LENGTH_SHORT).show();
+                }
+
+                btn6 = (Button) v;
+                valorBtn6 = btn6.getText().toString();
+            }
+        });
+
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(turno == false){
+                    btn7.setText("X");
+                    turno=true;
+                    btn7.setEnabled(false);
+                    Toast.makeText(GatoActivity.this, "Turno Jugador 2", Toast.LENGTH_SHORT).show();
+                }else{
+                    btn7.setText("O");
+                    turno=false;
+                    btn7.setEnabled(false);
+                    Toast.makeText(GatoActivity.this, "Turno Jugador 1", Toast.LENGTH_SHORT).show();
+                }
+
+                btn7 = (Button) v;
+                valorBtn7 = btn7.getText().toString();
+            }
+        });
+
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(turno == false){
+                    btn8.setText("X");
+                    turno=true;
+                    btn8.setEnabled(false);
+                    Toast.makeText(GatoActivity.this, "Turno Jugador 2", Toast.LENGTH_SHORT).show();
+                }else{
+                    btn8.setText("O");
+                    turno=false;
+                    btn8.setEnabled(false);
+                    Toast.makeText(GatoActivity.this, "Turno Jugador 1", Toast.LENGTH_SHORT).show();
+                }
+
+                btn8 = (Button) v;
+                valorBtn8 = btn8.getText().toString();
+            }
+        });
+
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(turno == false){
+                    btn9.setText("X");
+                    turno=true;
+                    btn9.setEnabled(false);
+                    Toast.makeText(GatoActivity.this, "Turno Jugador 2", Toast.LENGTH_SHORT).show();
+                }else{
+                    btn9.setText("O");
+                    turno=false;
+                    btn9.setEnabled(false);
+                    Toast.makeText(GatoActivity.this, "Turno Jugador 1", Toast.LENGTH_SHORT).show();
+                }
+
+                btn9 = (Button) v;
+                valorBtn9 = btn9.getText().toString();
+            }
+        });
+
+        if(valorBtn1.equals(valorBtn2) && valorBtn2.equals(valorBtn3)){
+            txtv_ganador.setText("Ganador: Jugador #");
+        }
+
+
+        /*if((valorBtn1.equals("X") && valorBtn2.equals("X") && valorBtn3.equals("X"))
+                || (valorBtn4.equals("X") && valorBtn5.equals("X") && valorBtn6.equals("X"))
+                || (valorBtn7.equals("X") && valorBtn8.equals("X") && valorBtn9.equals("X"))
+                || (valorBtn1.equals("X") && valorBtn4.equals("X") && valorBtn7.equals("X"))
+                || (valorBtn2.equals("X") && valorBtn5.equals("X") && valorBtn8.equals("X"))
+                || (valorBtn3.equals("X") && valorBtn6.equals("X") && valorBtn9.equals("X"))
+                || (valorBtn1.equals("X") && valorBtn5.equals("X") && valorBtn9.equals("X"))
+                || (valorBtn3.equals("X") && valorBtn5.equals("X") && valorBtn7.equals("X"))){
+
+            txtv_ganador.setText("Ganador: Jugador 1");
+
+            btn1.setEnabled(false);
+            btn2.setEnabled(false);
+            btn3.setEnabled(false);
+            btn4.setEnabled(false);
+            btn5.setEnabled(false);
+            btn6.setEnabled(false);
+            btn7.setEnabled(false);
+            btn8.setEnabled(false);
+            btn9.setEnabled(false);
+
+        }else if ((valorBtn1.equals("O") && valorBtn2.equals("O") && valorBtn3.equals("O"))
+                    || (valorBtn4.equals("O") && valorBtn5.equals("O") && valorBtn6.equals("O"))
+                    || (valorBtn7.equals("O") && valorBtn8.equals("O") && valorBtn9.equals("O"))
+                    || (valorBtn1.equals("O") && valorBtn4.equals("O") && valorBtn7.equals("O"))
+                    || (valorBtn2.equals("O") && valorBtn5.equals("O") && valorBtn8.equals("O"))
+                    || (valorBtn3.equals("O") && valorBtn6.equals("O") && valorBtn9.equals("O"))
+                    || (valorBtn1.equals("O") && valorBtn5.equals("O") && valorBtn9.equals("O"))
+                    || (valorBtn3.equals("O") && valorBtn5.equals("O") && valorBtn7.equals("O"))){
+
+            txtv_ganador.setText("Ganador: Jugador 2");
+
+            btn1.setEnabled(false);
+            btn2.setEnabled(false);
+            btn3.setEnabled(false);
+            btn4.setEnabled(false);
+            btn5.setEnabled(false);
+            btn6.setEnabled(false);
+            btn7.setEnabled(false);
+            btn8.setEnabled(false);
+            btn9.setEnabled(false);
+
+        }*/
 
     }
 
--- app/src/main/java/com/favio/gatoapp/MainActivity.java
+++ app/src/main/java/com/favio/gatoapp/MainActivity.java
@@ -1,212 +1,46 @@
 package com.favio.gatoapp;
 
+import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnJugar, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
-    boolean turno=false;
-
+    TextView txtv_contador;
+    int contador=7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnJugar=findViewById(R.id.btnJugar);
-        btn1=findViewById(R.id.btn1);
-        btn2=findViewById(R.id.btn2);
-        btn3=findViewById(R.id.btn3);
-        btn4=findViewById(R.id.btn4);
-        btn5=findViewById(R.id.btn5);
-        btn6=findViewById(R.id.btn6);
-        btn7=findViewById(R.id.btn7);
-        btn8=findViewById(R.id.btn8);
-        btn9=findViewById(R.id.btn9);
+        txtv_contador = findViewById(R.id.txtv_contador);
 
-        btn1.setEnabled(false);
-        btn2.setEnabled(false);
-        btn3.setEnabled(false);
-        btn4.setEnabled(false);
-        btn5.setEnabled(false);
-        btn6.setEnabled(false);
-        btn7.setEnabled(false);
-        btn8.setEnabled(false);
-        btn9.setEnabled(false);
+        new CountDownTimer(7000, 1000){
 
-        btnJugar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onTick(long millisUntilFinished) {
 
-                btn1.setEnabled(true);
-                btn2.setEnabled(true);
-                btn3.setEnabled(true);
-                btn4.setEnabled(true);
-                btn5.setEnabled(true);
-                btn6.setEnabled(true);
-                btn7.setEnabled(true);
-                btn8.setEnabled(true);
-                btn9.setEnabled(true);
-
-                btn1.setText(" ");
-                btn2.setText(" ");
-                btn3.setText(" ");
-                btn4.setText(" ");
-                btn5.setText(" ");
-                btn6.setText(" ");
-                btn7.setText(" ");
-                btn8.setText(" ");
-                btn9.setText(" ");
+                contador--;
+                txtv_contador.setText(Integer.toString(contador));
             }
-        });
 
-        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onFinish() {
 
-                if(turno == false){
-                    btn1.setText("X");
-                    turno=true;
-                    btn1.setEnabled(false);
-                }else{
-                    btn1.setText("O");
-                    turno=false;
-                    btn1.setEnabled(false);
-                }
+                Intent i_GatoActivity=new Intent(MainActivity.this, GatoActivity.class);
+                startActivity(i_GatoActivity);
+
+                finish();
             }
-        });
-
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if(turno == false){
-                    btn2.setText("X");
-                    turno=true;
-                    btn2.setEnabled(false);
-                }else{
-                    btn2.setText("O");
-                    turno=false;
-                    btn2.setEnabled(false);
-                }
-            }
-        });
-
-        btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if(turno == false){
-                    btn3.setText("X");
-                    turno=true;
-                    btn3.setEnabled(false);
-                }else{
-                    btn3.setText("O");
-                    turno=false;
-                    btn3.setEnabled(false);
-                }
-            }
-        });
-
-        btn4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if(turno == false){
-                    btn4.setText("X");
-                    turno=true;
-                    btn4.setEnabled(false);
-                }else{
-                    btn4.setText("O");
-                    turno=false;
-                    btn4.setEnabled(false);
-                }
-            }
-        });
-
-        btn5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if(turno == false){
-                    btn5.setText("X");
-                    turno=true;
-                    btn5.setEnabled(false);
-                }else{
-                    btn5.setText("O");
-                    turno=false;
-                    btn5.setEnabled(false);
-                }
-            }
-        });
-
-        btn6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if(turno == false){
-                    btn6.setText("X");
-                    turno=true;
-                    btn6.setEnabled(false);
-                }else{
-                    btn6.setText("O");
-                    turno=false;
-                    btn6.setEnabled(false);
-                }
-            }
-        });
-
-        btn7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if(turno == false){
-                    btn7.setText("X");
-                    turno=true;
-                    btn7.setEnabled(false);
-                }else{
-                    btn7.setText("O");
-                    turno=false;
-                    btn7.setEnabled(false);
-                }
-            }
-        });
-
-        btn8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if(turno == false){
-                    btn8.setText("X");
-                    turno=true;
-                    btn8.setEnabled(false);
-                }else{
-                    btn8.setText("O");
-                    turno=false;
-                    btn8.setEnabled(false);
-                }
-            }
-        });
-
-        btn9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if(turno == false){
-                    btn9.setText("X");
-                    turno=true;
-                    btn9.setEnabled(false);
-                }else{
-                    btn9.setText("O");
-                    turno=false;
-                    btn9.setEnabled(false);
-                }
-            }
-        });
+        }.start();
 
     }
 
 }
+
